1a77684cf024ac4f32e0088637c9f2f880b3e35f
nguyen dinh chien <Ndc7571@gmail.com>
2024-04-01 19:15:57 +0700

    boostrap login  and register

--- Register/register.php
+++ Register/register.php
@@ -1,7 +1,3 @@
-<?php
-require_once ("check_register.php")
-     ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,65 +5,63 @@ require_once ("check_register.php")
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Register</title>
+     <!-- Thêm các tệp CSS của Bootstrap -->
+     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-     <h1>Registration Form</h1>
-     <form action="" method="post">
-          <div>
-               <strong>Full name:</strong>
-               <br>
-               <input type="text" name="full_name"
-                    value="<?php echo isset($_POST['full_name']) ? htmlspecialchars($_POST['full_name']) : ''; ?>">
-          </div>
-          <div>
-               <strong>Username:</strong>
-               <br>
-               <input type="text" name="username"
-                    value="<?php echo isset($_POST['username']) ? htmlspecialchars($_POST['username']) : ''; ?>">
-          </div>
-          <div>
-               <strong>Email:</strong>
-               <br>
-               <input type="email" name="email"
-                    value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
-          </div>
-          <div>
-               <strong>Date Of Birth:</strong>
-               <br>
-               <input name="dob" type="date"
-                    value="<?php echo isset($_POST['dob']) ? htmlspecialchars($_POST['dob']) : ''; ?>">
-          </div>
-          <div>
-               <strong>Gender:</strong>
-               <br>
-               <div>
-                    <input type="radio" name="gender" value="1">Nam
-                    <input type="radio" name="gender" value="2">Nữ
-                    <input type="radio" name="gender" value="3">Khác
+     <div class="container">
+          <h1 class="mt-5">Registration Form</h1>
+          <form action="" method="post" class="mt-3">
+               <div class="form-group">
+                    <label for="full_name">Full name:</label>
+                    <input type="text" class="form-control" id="full_name" name="full_name"
+                         value="<?php echo isset($_POST['full_name']) ? htmlspecialchars($_POST['full_name']) : ''; ?>">
                </div>
-          </div>
-          <div>
-               <strong>Password:</strong>
-               <br>
-               <input type="password" name="password">
-          </div>
-          <div>
-               I agree to the conditions:
-               <br>
-               <input type="checkbox" name="condition" value="Accept">
-          </div>
-          <div>
-               <input type="submit" name="submit" value="Submit">
-               <a href="../login/login.php">Login</a>
-          </div>
-     </form>
+               <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username"
+                         value="<?php echo isset($_POST['username']) ? htmlspecialchars($_POST['username']) : ''; ?>">
+               </div>
+               <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email"
+                         value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
+               </div>
+               <div class="form-group">
+                    <label for="dob">Date Of Birth:</label>
+                    <input type="date" class="form-control" id="dob" name="dob"
+                         value="<?php echo isset($_POST['dob']) ? htmlspecialchars($_POST['dob']) : ''; ?>">
+               </div>
+               <div class="form-group">
+                    <label for="gender">Gender:</label>
+                    <div>
+                         <input type="radio" id="male" name="gender" value="1" class="mr-2">
+                         <label for="male">Nam</label>
+                         <input type="radio" id="female" name="gender" value="2" class="ml-3 mr-2">
+                         <label for="female">Nữ</label>
+                         <input type="radio" id="other" name="gender" value="3" class="ml-3 mr-2">
+                         <label for="other">Khác</label>
+                    </div>
+               </div>
+               <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password">
+               </div>
+               <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="condition" name="condition" value="Accept">
+                    <label class="form-check-label" for="condition">I agree to the conditions</label>
+               </div>
+               <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+               <a href="../login/login.php" class="btn btn-link">Login</a>
+          </form>
 
-     <?php
-     if (isset($check)) {
-          echo $check;
-     }
-     ?>
+          <?php
+          if (isset($check)) {
+               echo $check;
+          }
+          ?>
+     </div>
 
 
 </body>
--- login/login.php
+++ login/login.php
@@ -1,48 +1,56 @@
 <?php
-require_once ("check_login.php")
-     ?>
+require_once ("check_login.php");
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
      <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Login</title>
+     <!-- Bootstrap CSS -->
+     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+     <style>
+          /* Add your custom styles here */
+     </style>
 </head>
 
 <body>
-     <form action="" method="post">
-          <table>
-               <tr>
-                    <td>Username:</td>
-                    <td>
-                         <input type="text" name="username">
-                    </td>
-               </tr>
-               <tr>
-                    <td>Password:</td>
-                    <td>
-                         <input type="password" name="password">
-                    </td>
-               </tr>
-               <tr>
-                    <td>
-                         <input type="submit" name="submit" value="Login">
-                    </td>
-                    <td>
-                         <a href="../Register/register.php">Register</a>
-                    </td>
-               </tr>
-          </table>
-     </form>
+     <div class="container mt-5">
+          <form action="" method="post">
+               <div class="form-group row">
+                    <label for="username" class="col-sm-2 col-form-label">Username:</label>
+                    <div class="col-sm-10">
+                         <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+               </div>
+               <div class="form-group row">
+                    <label for="password" class="col-sm-2 col-form-label">Password:</label>
+                    <div class="col-sm-10">
+                         <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+               </div>
+               <div class="form-group row">
+                    <div class="col-sm-10 offset-sm-2">
+                         <input type="submit" class="btn btn-primary" name="submit" value="Login">
+                    </div>
+               </div>
+          </form>
+          <div class="row">
+               <div class="col-sm-10 offset-sm-2">
+                    <a href="../Register/register.php" class="btn btn-link">Register</a>
+               </div>
+          </div>
 
-     <?php
-     if (isset($check)) {
-          echo $check;
-     }
-     ?>
+          <?php
+          if (isset($check)) {
+               echo '<div class="alert alert-danger mt-3" role="alert">' . $check . '</div>';
+          }
+          ?>
+     </div>
+
+
 </body>
 
 </html>
